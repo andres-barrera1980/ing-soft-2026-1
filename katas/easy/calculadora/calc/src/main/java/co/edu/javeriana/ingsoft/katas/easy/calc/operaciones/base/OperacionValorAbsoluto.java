b0405ae46cb4502ffe32f1fdb75d9063ca23cdc1
Juan Suarez <suarez-jpablo@javeriana.edu.co>
2026-03-02 19:35:29 -0500

    Codigo

--- katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/OperacionValorAbsoluto.java
+++ katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/OperacionValorAbsoluto.java
@@ -1,5 +1,16 @@
 package co.edu.javeriana.ingsoft.katas.easy.calc.operaciones.base;
 
-public class OperacionValorAbsoluto {
+public class OperacionValorAbsoluto  {
+    public Integer realizarOperacion(Integer num) {
+        if (num == null) {
+            throw new IllegalArgumentException("El número no puede ser nulo");
+        }
 
+        // Caso especial: overflow
+        if (num.equals(Integer.MIN_VALUE)) {
+            throw new ArithmeticException("integer overflow");
+        }
+
+        return (num >= 0) ? num : -num;
+    }
 }

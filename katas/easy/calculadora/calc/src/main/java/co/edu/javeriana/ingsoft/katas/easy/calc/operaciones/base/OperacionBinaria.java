267937f37a95fad2dd21510aad986b988fe22cc9
Mateo <matxx5789@gmail.com>
2026-03-02 19:40:59 -0500

    Correccion con dos clases Traslaviña/Torres

--- katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/OperacionBinaria.java
+++ katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/OperacionBinaria.java
@@ -1,6 +1,5 @@
 package co.edu.javeriana.ingsoft.katas.easy.calc.operaciones.base;
 
 public abstract class OperacionBinaria {
-
     public abstract Integer realizarOperacion(Integer num1, Integer num2);
 }

3507e3947847a0e9d314b31acc94c8d1a9a1c283
Mateo <matxx5789@gmail.com>
2026-03-02 19:06:43 -0500

    test: Ciclo2 Green

--- katas/easy/calculadora/calc/src/test/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/MultiplicacionTest.java
+++ katas/easy/calculadora/calc/src/test/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/MultiplicacionTest.java
@@ -1,7 +1,12 @@
 package co.edu.javeriana.ingsoft.katas.easy.calc.operaciones.base;
 
+import com.sun.jdi.IntegerType;
+
 public class MultiplicacionTest {
     public int Multiplicar (int a, int b){ //Metodo para multiplicar numeros enteros
-        return a*b;
+        long resultado= (long) a*b;
+        if (resultado< Integer.MIN_VALUE || resultado>Integer.MAX_VALUE)
+            throw new ArithmeticException("Hay desbordamiento en el codigo.");
+        return (int) resultado;
     }
 }

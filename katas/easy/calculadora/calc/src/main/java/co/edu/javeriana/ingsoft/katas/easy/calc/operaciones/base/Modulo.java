2c2f04adefdaa015e0603c4c2af3ba87f0106429
Marlon <mjgarcia@javeriana.edu.co>
2026-03-02 20:28:16 -0500

    feat: implementacion modulo

--- katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/Modulo.java
+++ katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/Modulo.java
@@ -1,9 +1,18 @@
 package co.edu.javeriana.ingsoft.katas.easy.calc.operaciones.base;
 
 public  class Modulo extends OperacionBinaria{
-    @Override
-    public  Integer realizarOperacion(Integer num1, Integer num2){
-        return num1%num2;
+   @Override
+    public Integer realizarOperacion(Integer num1, Integer num2) {
+
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Los valores no pueden ser null.");
+        }
+
+        if (num2 == 0) {
+            throw new ArithmeticException("No se puede dividir por 0.");
+        }
+
+        return num1 % num2;
     }
 
 }

ef8af2425c009726c2aed565fdf3e24fc79ccedf
Samuel López <samuellopez@javeriana.edu.co>
2026-03-02 20:30:32 -0500

    fix: elimina metodos duplicados y corrige validacion de overflow en division

--- katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/DivisionSegura.java
+++ katas/easy/calculadora/calc/src/main/java/co/edu/javeriana/ingsoft/katas/easy/calc/operaciones/base/DivisionSegura.java
@@ -1,18 +1,19 @@
 package co.edu.javeriana.ingsoft.katas.easy.calc.operaciones.base;
 
-public abstract class DivisionSegura extends OperacionBinaria {
+public class DivisionSegura extends OperacionBinaria {
 
-    public double dividir(double numerador, double divisor) {
+    @Override
+    public Integer realizarOperacion(Integer num1, Integer num2) {
 
-        if (numerador < Integer.MIN_VALUE || numerador > Integer.MAX_VALUE ||
-                divisor < Integer.MIN_VALUE || divisor > Integer.MAX_VALUE) {
-            throw new IllegalArgumentException("Los valores deben estar dentro del rango de 32 bits.");
+        if (num2 == 0) {
+            throw new ArithmeticException("No es posible dividir por cero");
         }
 
-        if (divisor == 0) {
-            throw new ArithmeticException("No se puede dividir por cero.");
+
+        if (num1 == Integer.MIN_VALUE && num2 == -1) {
+            throw new IllegalArgumentException("Valor fuera del rango de 32 bits (Overflow)");
         }
 
-        return (double) numerador / divisor;
+        return num1 / num2;
     }
 }

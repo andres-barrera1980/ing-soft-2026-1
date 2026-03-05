a4a8a7e8361aae76e77d3a74cb9f82a385d7eded
Juan Rodriguez <juandrodriguezf@javeriana.edu.co>
2026-03-04 22:38:30 -0500

    refactor: valida numeros positivos

--- calculadora/src/Calculadora.java
+++ calculadora/src/Calculadora.java
@@ -1,6 +1,11 @@
 public class Calculadora {
 
     public static double log10(double x) {
+
+        if (x <= 0) {
+            throw new IllegalArgumentException("Numero invalido");
+        }
+
         return Math.log10(x);
     }
 
